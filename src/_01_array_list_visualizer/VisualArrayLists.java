8110649c018bca66941944f22fcc7c7862b3a2aa
dshan102 <53541391+dshan102@users.noreply.github.com>
2020-12-18 18:12:42 -0800

    VisualArrayLists (FINISHED)

--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -16,21 +16,30 @@ public class VisualArrayLists implements ActionListener {
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+		list.add("Stuff");
+		list.add("Thing");
+		list.add("Object");
+		list.add("Item");
 		// 2. Set one item in our list to be something different
-		
+		list.set(1, "something");
 		// 3. Remove something from our list
-		
+		list.remove(1);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		if(list.contains("Junk")) {
+			System.out.println("There is junk in this list!");
+		}
+		else {
+			System.out.println("This list is free of junk!");
+		}
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(2));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
